24e7dad8c14604d145d1c1f9ffb35ada8fcadaf5
cacard <cacard@126.com>
2015-11-13 19:00:41 +0800

    -

---
+++ app/src/main/java/com/cacard/demo/Drawable/SpecialNinePatchActivity.java
@@ -0,0 +1,14 @@
+package com.cacard.demo.Drawable;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by cunqingli on 2015/11/10.
+ */
+public class SpecialNinePatchActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
